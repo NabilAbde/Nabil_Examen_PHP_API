2f4e9dcc2f5d9ba58545c681ade26331869fecbc
[nabil] <[consonab@gmail.com]>
2021-06-25 16:53:08 +0100

    modif manquait une parentese

--- models/film.php
+++ models/film.php
@@ -92,7 +92,8 @@ class Film
                     break;
             endswitch;
         }
-    }public function AddFilm($idCategory, $title, $resume)
+    }
+    public function AddFilm($idCategory, $title, $resume)
     {
 
         $servername = 'localhost';
@@ -109,10 +110,10 @@ class Film
             $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             $sql = "INSERT INTO film (idCategory,title, resume) VALUES ('$idCategory','$title', '$resume)";
             $stmt = $dbc->prepare($sql);
-            $stmt->execute(['$idCategory','$title', '$resume]);
+            $stmt->execute(['$idCategory', '$title', '$resume']);
         } catch (PDOException $e) {
 
-
+            echo $e->getMessage();
             switch ($e->getCode()):
                 case 2002:
                     echo "Erreur de connexion à la base de données";
@@ -126,41 +127,4 @@ class Film
             endswitch;
         }
     }
-
-    public function Delete($id)
-    {
-        $servername = 'localhost';
-        $bddname = "shoptoncafe";
-        $username = 'root';
-        $password = '';
-
-
-
-        try {
-            $dbc = new PDO("mysql:host=$servername;dbname=$bddname", $username, $password);
-            echo "Connexion à la BDD établie";
-
-            $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-            $sql = "DELETE FROM product WHERE idProduct = $id";
-            $stmt = $dbc->query($sql)->fetch();
-            //$stmt->exec($sql);
-        } catch (PDOException $e) {
-
-
-            switch ($e->getCode()):
-                case 2002:
-                    echo "Erreur de connexion à la base de données";
-                    break;
-                case 'HY000':
-                    echo "Attention la BDD existe déjà";
-                    break;
-                default:
-                    echo $e->getCode();
-                    break;
-            endswitch;
-        }
-    }
-}
-
 }
